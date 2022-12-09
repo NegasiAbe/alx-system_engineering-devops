50ad25e04858489635abc6d9da119823cefac6ed
NegasiAbe <61984619+NegasiAbe@users.noreply.github.com>
2022-12-09 23:03:42 +0100

    solution added

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,8 @@
-helloe
+# This manifest creates a file at /tmp
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
